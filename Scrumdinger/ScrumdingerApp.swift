17ae2a6bdd64d5c4b53deac0fd9cf2a915f664a6
Stephen Dawes <sdawes@outlook.com>
2023-09-22 11:45:01 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Stephen Dawes on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
